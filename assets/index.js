072cd0cf16331030028d57fff851811e5352fa25
Jeff Knaggs <mail@jeef3.com>
2024-11-06 12:02:42 +1300

    Update index cards on manual scroll

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,43 @@
 (function () {
-  const carousel = document.querySelector("[data-carousel]");
-  const scroller = carousel.querySelector("[data-carousel-scroller]");
+  const carouselEl = document.querySelector("[data-carousel]");
 
-  const idx = carousel.querySelector("[data-carousel-index]");
+  const scrollerEl = carouselEl.querySelector("[data-carousel-scroller]");
 
-  const buttons = idx.querySelectorAll("[data-carousel-index-card]");
-  buttons.forEach((bt) => bt.addEventListener("click", handleIdxCardClick));
+  const indexEl = carouselEl.querySelector("[data-carousel-index]");
+  const indexButtonEls = indexEl.querySelectorAll("[data-carousel-index-card]");
+
+  scrollerEl.addEventListener("scroll", handleScrollerScroll);
+  indexButtonEls.forEach((bt) =>
+    bt.addEventListener("click", handleIdxCardClick),
+  );
+
+  let debouncer = null;
+
+  function handleScrollerScroll() {
+    if (debouncer) {
+      clearTimeout(debouncer);
+    }
+
+    debouncer = setTimeout(() => {
+      debouncer = null;
+      scrollerEl.querySelectorAll("[data-carousel-card]").forEach((el) => {
+        if (el.offsetLeft === scrollerEl.scrollLeft) {
+          const target = el.getAttribute("data-carousel-card");
+
+          indexButtonEls.forEach((bt) => bt.classList.remove("active"));
+          indexEl
+            .querySelector(`[data-carousel-index-card="${target}"]`)
+            .classList.add("active");
+        }
+      });
+    }, 200);
+  }
 
   function handleIdxCardClick(e) {
     const show = e.currentTarget.getAttribute("data-carousel-index-card");
 
-    buttons.forEach((bt) => bt.classList.remove("active"));
+    const fig = carouselEl.querySelector(`[data-carousel-card="${show}"]`);
 
-    e.currentTarget.classList.add("active");
-
-    const fig = carousel.querySelector(`[data-carousel-card="${show}"]`);
-
-    scroller.scrollTo({ left: fig.offsetLeft, behavior: "smooth" });
+    scrollerEl.scrollTo({ left: fig.offsetLeft, behavior: "smooth" });
   }
 })();
