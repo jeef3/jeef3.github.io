8afdcaff3d146709b9847969adc358c003b47e40
Jeff Knaggs <mail@jeef3.com>
2024-11-05 16:42:32 +1300

    Super basic carousel functionality

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,21 @@
 (function () {
   const carousel = document.querySelector("[data-carousel]");
+  const scroller = carousel.querySelector("[data-carousel-scroller]");
+
+  const idx = carousel.querySelector("[data-carousel-index]");
+
+  const buttons = idx.querySelectorAll("[data-carousel-index-card]");
+  buttons.forEach((bt) => bt.addEventListener("click", handleIdxCardClick));
+
+  function handleIdxCardClick(e) {
+    const show = e.currentTarget.getAttribute("data-carousel-index-card");
+
+    buttons.forEach((bt) => bt.classList.remove("active"));
+
+    e.currentTarget.classList.add("active");
+
+    const fig = carousel.querySelector(`[data-carousel-card="${show}"]`);
+
+    scroller.scrollTo({ left: fig.offsetLeft, behavior: "smooth" });
+  }
 })();
